002cbd491f1e32b78b280dcf38cf60580b34e269
limingth <limingth@gmail.com>
2019-06-25 16:18:14 +0800

    Initial commit of a Simplest React App

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://maodou.io"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn Maodou
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
