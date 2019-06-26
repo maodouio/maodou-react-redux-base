6d1ff815bc6c8f7ad0984e87d35d91e52a69f63d
limingth <limingth@gmail.com>
2019-06-26 15:37:40 +0800

    Add a button to handle AddCourse

--- src/containers/CoursesContainer.js
+++ src/containers/CoursesContainer.js
@@ -25,18 +25,37 @@ class CoursesContainer extends Component {
 		})
 	}
 
+	handleAddCourse(course) {
+		console.log('[handleAddCourse()] course', course)
+		var newCourses = this.state.courses
+		newCourses.push(course)
+		console.log('[handleAddCourse()] newCourses', newCourses)
+		this.setState({
+			courses: newCourses
+		})
+	}
+
 	render() {
 		const { courses } = this.state
+		const course = {"_id":"abc", "title":"Class 4", "members": ["4a", "4b"]}
+		course._id = new Date().toLocaleString()
+
 		return (
-			<CoursesList name='Maodou-Classes'>
-				{courses.map(course =>
-					<CourseItem
-						key={course._id}
-						course={course}
-						handleClick={(e) => this.handleClick(course, e)}
-					/>
-				)}
-			</CoursesList>
+			<div>
+				<CoursesList name='Maodou-Classes'>
+					{courses.map(course =>
+						<CourseItem
+							key={course._id}
+							course={course}
+							handleClick={(e) => this.handleClick(course, e)}
+						/>
+					)}
+				</CoursesList>
+				<hr/>
+				<button onClick={(e) => this.handleAddCourse(course)}>
+					添加测试课程
+				</button>
+			</div>
 		)
 	}
 }
