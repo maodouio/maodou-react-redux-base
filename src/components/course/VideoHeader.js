ff4a5efb1ab2cbbe405dc7707ee8e71abed64a90
limingth <limingth@gmail.com>
2020-03-24 11:36:19 +0800

    Change headerWrap to 1 line instead 2 lines as maodouketang

--- src/components/course/VideoHeader.js
+++ src/components/course/VideoHeader.js
@@ -4,11 +4,9 @@ import styles from './course.module.scss'
 export default ({ course }) => {
   return (
     <div className={styles.headerWrap}>
-      <div className={styles.title}>
-        {course.name}
-        <div className={styles.time}>
-          开始时间：{course.startTime && course.startTime.split('T')[0]}
-        </div>
+      <div className={styles.title}>{course.name}</div>
+      <div className={styles.time}>
+        开始时间：{course.startTime && course.startTime.split('T')[0]}
       </div>
     </div>
   )
