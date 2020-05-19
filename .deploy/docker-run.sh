c782712e07e26435bb3176fac4dd4d46d59c0bb3
zhaoic <zhaoic@163.com>
2020-05-19 17:21:29 +0800

    change url to demo.maodoulive.com

--- .deploy/docker-run.sh
+++ .deploy/docker-run.sh
@@ -4,6 +4,7 @@ docker run \
        -d \
        --name=maodou-react-redux-base \
        --restart=always \
+	-e VIRTUAL_HOST=demo.maodoulive.com \
        -p 8800:5000 \
        -e TZ=Asia/Shanghai \
        maodou-react-redux-base:latest
