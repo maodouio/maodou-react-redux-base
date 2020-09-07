1cc80a9ff0900c4c532801588a7b9a4b19e22208
limingth <limingth@gmail.com>
2020-09-07 14:27:00 +0800

    Fix bug of hls_sd 512k missing use hls_ld 256k instead

--- src/api/config.js
+++ src/api/config.js
@@ -1,2 +1,3 @@
 // export const API_URL = 'https://smh.maodou.io/api'
-export const API_URL = 'https://xichuang.maodoulive.com/api'
+// export const API_URL = 'https://xichuang.maodoulive.com/api'
+export const API_URL = 'https://live.maodoulive.com/api'
--- src/components/course/LiveVideo.js
+++ src/components/course/LiveVideo.js
@@ -5,8 +5,11 @@ import styles from './course.module.scss'
 export default class LiveVideo extends Component {
   render() {
     const { course } = this.props
-    const url = course.liveUrl.hls_sd
+    console.log('Live course', course)
+    // const url = course.liveUrl.hls_sd
+    const url = course.liveUrl.hls_ld
 
+    console.log('Live url', url)
     return (
       <div className={styles.wrap}>
         <VideoPlayer src={url} live={true} hidePlaybackRates={true} poster={''} />
